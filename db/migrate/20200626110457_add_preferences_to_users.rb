9d1c8b21d08e40151d7f22b2055c201ec752c945
Rui Baltazar <rui.p.baltazar@gmail.com>
2020-06-26 23:37:37 +0800

    example with hstore

--- db/migrate/20200626110457_add_preferences_to_users.rb
+++ db/migrate/20200626110457_add_preferences_to_users.rb
@@ -1,6 +1,6 @@
 class AddPreferencesToUsers < ActiveRecord::Migration[6.0]
   def change
-    enable_extension "hstore"
+    enable_extension 'hstore' unless extension_enabled?('hstore')
     add_column :users, :preferences, :hstore
     add_index :users, :preferences, using: :gin
   end
